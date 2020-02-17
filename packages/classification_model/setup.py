e905b3651c010701fa9b894935c73f7f7084b0b5
Neetu Murmu <neetumurmu1906@gmail.com>
2020-02-17 23:23:34 +0530

    change setup.py

--- packages/classification_model/setup.py
+++ packages/classification_model/setup.py
@@ -42,7 +42,7 @@ except FileNotFoundError:
 
 # Load the package's __version__.py module as a dictionary.
 ROOT_DIR = Path(__file__).resolve().parent
-PACKAGE_DIR = ROOT_DIR / NAME
+PACKAGE_DIR = ROOT_DIR / 'classification_model'
 about = {}
 with open(PACKAGE_DIR / 'VERSION') as f:
     _version = f.read().strip()
