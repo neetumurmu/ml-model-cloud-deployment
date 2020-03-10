bb8b2a545e9a99d6f36a66125e100cdeb672e976
Neetu Murmu <neetumurmu1906@gmail.com>
2020-03-10 10:02:57 +0530

    ml api test

--- packages/ml_api/tests/test_controller.py
+++ packages/ml_api/tests/test_controller.py
@@ -34,12 +34,12 @@ def test_prediction_endpoint_returns_prediction(flask_test_client):
     print(post_json)
     # When
     response = flask_test_client.post('/v1/predict/classification',
-                                      json=post_json)
+                                      json=json.loads(post_json))
 
     # Then
     assert response.status_code == 200
     response_json = json.loads(response.data)
     prediction = response_json['predictions']
     response_version = response_json['version']
-    assert prediction[0] == 5
+    assert prediction[0] == 2
     assert response_version == _version
